db368c7a84a61b49d697f4f1cfa75a84eac32846
luizH <luizhenriquefbb@gmail.com>
2018-03-10 16:55:10 -0300

    Merge sort end QuickSort

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# cc-APA-OrderingAlgorithms
+# cc-APA-SortingAlgorithms
 An APA activity.
-A simple project that sorts a list of numbers by inserion sort and selection sort
+A simple project that sorts a list of numbers by inserion sort , selection sort , merge and quick sort
 
 ## How to use
 place a file named "couting.txt" in the file project and enter the following command in the cmd or terminal
--- src/sort/Main.java
+++ src/sort/Main.java
@@ -11,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         MyCollection collection1 = new MyCollection("couting.txt");
         MyCollection collection2 = new MyCollection("couting.txt");
+        MyCollection collection3 = new MyCollection("couting.txt");
+        MyCollection collection4 = new MyCollection("couting.txt");
         
         System.out.println("Unsorted List");
         System.out.println(collection1);
@@ -22,6 +24,14 @@ public class Main {
         System.out.println("\nSorted by insertionSort");
         collection2.insertionSort();
         System.out.println(collection2);
+        
+        System.out.println("\nSorted by mergeSort");
+        collection3.mergeSort();
+        System.out.println(collection3);
+        
+        System.out.println("\nSorted by quickSort");
+        collection4.quickSort();
+        System.out.println(collection4);
     }
     
 }
