d7e2221460853c5b103e549cb39efa55f75f4839
luizH <luizhenriquefbb@gmail.com>
2018-03-10 16:55:10 -0300

    Merge sort and QuickSort

--- src/sort/MyCollection.java
+++ src/sort/MyCollection.java
@@ -12,20 +12,7 @@ public class MyCollection {
 
     private List<Integer> myCollection;
 
-    /**
-     * initialize list with random values
-     */
-    public MyCollection() {
-        this.myCollection = new ArrayList<>();
-        this.myCollection.add(2);
-        this.myCollection.add(5);
-        this.myCollection.add(10);
-        this.myCollection.add(237);
-        this.myCollection.add(53);
-        this.myCollection.add(0);
-        this.myCollection.add(7);
-    }
-
+ 
     /**
      * Read from a file txt. <br>
      * File format: One number per line
@@ -99,7 +86,141 @@ public class MyCollection {
         }
 
     }
+    
+    /**
+     * Main method: the object of study
+     * <p>
+     * datapted from: <a href="http://www.java2novice.com/java-sorting-algorithms/merge-sort/">
+     * this site</a>
+     * </p>
+     */
+    public void mergeSort(){
+        this.splitAndMerge(0, this.myCollection.size()-1);
+    }
+    
+    /**
+     * recursive method
+     * @param beginList beginning of the list
+     * @param endList end of the list
+     */
+    private void splitAndMerge(int beginList, int endList){
+        int middle = (beginList+(endList))/2;
+        int[] temp = new int[this.myCollection.size()];
+        // stop condition (array size == 1)
+        if(beginList < endList){
+            
+            //left side
+            splitAndMerge(beginList, middle);
+            //right side
+            splitAndMerge(middle+1, endList);
+           //merging
+            merge(beginList, middle, endList, temp);
+            
+        }
+    }
+    
+    /**
+     * Merge two arrays (in this case, two lists) based on their indexes
+     * @param beginList
+     * @param middle
+     * @param endList 
+     * @param tempArray temporary auxiliar array
+     */
+    private void merge(int beginList,int middle,int endList, int[] tempArray){
+        int indexLeft = beginList;
+        int indexRight = middle+1;
+        
+        int iTemp = beginList; // position on 'tempArray'
+        
+        int numElements = endList - beginList + 1;
 
+        
+        while(indexLeft <= middle && indexRight <= endList ){
+            // compare values of left and right
+            if(this.myCollection.get(indexLeft) <= this.myCollection.get(indexRight)){
+                // put left value 
+                tempArray[iTemp] = this.myCollection.get(indexLeft);
+                indexLeft++;
+            }else{
+                // put right value
+                tempArray[iTemp] = this.myCollection.get(indexRight);
+                indexRight++;
+            }
+            iTemp++;
+        }
+        
+        // remains values
+        while(indexLeft <= middle){
+            tempArray[iTemp] = this.myCollection.get(indexLeft);
+            indexLeft++;
+            iTemp++;
+        }
+        
+        while(indexRight <= endList){
+            tempArray[iTemp] = this.myCollection.get(indexRight);
+            indexRight++;
+            iTemp++; 
+        }
+        
+        // coppying tempArray to list
+         for( int i = 0; i < numElements; i++, endList-- ){
+            this.myCollection.set(endList, tempArray[ endList ]);
+        }
+
+        
+    }
+
+    /**
+     * Main method: the object of study
+     */
+    public void quickSort(){
+        quick(0, this.myCollection.size()-1);
+    }
+    
+    private void quick(int beginList, int endList){
+        //stop condition
+        if (beginList < endList) {
+            int pivot;
+            pivot = this.splitAndQuick(beginList, endList);
+            quick(beginList, pivot - 1);
+            quick(pivot + 1, endList);
+        }
+    }
+    
+    /**
+     * @param beginList
+     * @param endList
+     * @return 
+     */
+    private int splitAndQuick(int beginList, int endList) {
+        /** by convention, pivot is the first element */
+        int pivot = this.myCollection.get(beginList);
+        int b = beginList + 1, e = endList;
+        // until the indexes cross
+        while (b <= e) {
+            // left side must be less than the pivot
+            if (this.myCollection.get(b) <= pivot) {
+                b++;
+                
+            // right side must be greater than the pivot
+            } else if (pivot < this.myCollection.get(e)) {
+                e--;
+            
+            //if one of the condiotions above fails swap elements of the indexes
+            } else {
+                int swap = this.myCollection.get(b);
+                this.myCollection.set(b, this.myCollection.get(e));
+                this.myCollection.set(e, swap);
+                b++;
+                e--;
+            }
+        }
+        // put pivot in the middle and return the middle position
+        this.myCollection.set(beginList, this.myCollection.get(e));
+        this.myCollection.set(e, pivot);
+        return e;
+    }
+        
     @Override
     public String toString() {
         return "MyCollection{\n"
